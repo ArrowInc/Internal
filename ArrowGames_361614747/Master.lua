9a6e00493ebd13cc2710ceaf7803623371b56e81
RafDevRBLX <RafDevRBLX@users.noreply.github.com>
2016-03-12 11:20:54 +0000

    Update Master.lua

--- ArrowGames_361614747/Master.lua
+++ ArrowGames_361614747/Master.lua
@@ -1132,8 +1132,12 @@ function HandleRemoteCommand(cmd)
 	meta.__index.isRemote = true
 	
 	coroutine.wrap(function() pcall(onChat,false,fakePlr,cmd.Command) end)()
-	delay(cmd.ServerKey and .1 or 10,function()
-		pcall(function() game:GetService('HttpService'):GetAsync('http://www.rsoindustries.com/Arrow/ArrowRemote/handlecmd.php?action=clear&cmdId=' . tostring(cmd.CmdId) . '&token=' . ====4====) end)
+	local dl = .1
+	if(cmd.ServerKey==nil) then dl=10 end
+	delay(dl,function()
+		pcall(function()
+			game:GetService('HttpService'):GetAsync('http://www.rsoindustries.com/Arrow/ArrowRemote/handlecmd.php?action=clear&cmdId=' . tostring(cmd.CmdId) . '&token=' . ====4====)
+		end)
 	end)
 end
 
