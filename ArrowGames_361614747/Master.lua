3c68bf757a68f95c1ed00acbeeec29ac759756ff
RafDevRBLX <RafDevRBLX@users.noreply.github.com>
2016-03-12 10:27:55 +0000

    GenStr

--- ArrowGames_361614747/Master.lua
+++ ArrowGames_361614747/Master.lua
@@ -137,6 +137,22 @@ local RecentChatLog = {}
 
 local PlayerData = {}
 
+local function GenStr(len)
+		len = len or 10
+		local s = ''
+		local Range = {
+			'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+			'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+			'1','2','3','4','5','6','7','8','9','0'
+		}
+		math.randomseed(os.time())
+		for i=1,len do
+			s=s..Range[math.random(1,#Range)]
+		end
+		
+		return s
+	end
+
 local function GetDate (TimeStamp)
 	TimeStamp = TimeStamp or os.time()
 	local z = math.floor(TimeStamp / 86400) + 719468
