48978ac78c8c30cb5361d25086dadc0fa3462144
RafDevRBLX <RafDevRBLX@users.noreply.github.com>
2016-03-21 18:51:32 +0000

    Update Master.lua

--- ArrowGames_361614747/Master.lua
+++ ArrowGames_361614747/Master.lua
@@ -1179,9 +1179,9 @@ local function ConnectPlayer(plr)
 	pcall(function() level.Value = pData["Level"] end)
 	
 	--CSB:FireClient(plr,'SetupLocalPart',game:GetService('Lighting'):FindFirstChild('LocalDoor'))
-	if plr:IsInGroup(2757514) then
+	--[[if plr:IsInGroup(2757514) then
 		CSB:FireClient(plr,'SetupLocalTransparency',workspace.Lobby.SpecialPart,1)
-	end
+	end]]
 	
 	Hint(cHint,plr)
 	
