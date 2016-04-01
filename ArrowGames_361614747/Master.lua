877cbe55970b6706cd07535a72b98d05afa1ac5f
RafDevRBLX <RafDevRBLX@users.noreply.github.com>
2016-04-01 23:48:01 +0100

    Update Master.lua

--- ArrowGames_361614747/Master.lua
+++ ArrowGames_361614747/Master.lua
@@ -81,7 +81,6 @@ local Admins = {
 	['10469346'] = 'chillybagpiper',
 	['55305559'] = 'Fireball1850',
 	['57264590'] = 'httpAbbey',
-	['75750606'] = 'figuretjetsalt', -- TRIAL (Start:3/23/16)
 	['32032357'] = 'Scriptey',
 	['25307609'] = 'dwong',
 }
